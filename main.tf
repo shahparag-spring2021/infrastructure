bb5a8ae289606d52b827571d1e990fab4935c241
Parag Shah <paragshah367@gmail.com>
2021-03-18 00:46:17 -0700

    Final changes, ci / cd, adding gh-ec2-ami

--- main.tf
+++ main.tf
@@ -450,6 +450,65 @@ resource "aws_iam_policy" "GH-Upload-To-S3" {
 EOF
 }
 
+//attachment of GH-Upload-To-S3 IAM Policy to ghactions_user
+resource "aws_iam_user_policy_attachment" "ghactions_ec2_ami" {
+  user       = data.aws_iam_user.ghactions_user.user_name
+  policy_arn = aws_iam_policy.gh-ec2-ami.arn
+}
+
+# GH-Upload-To-S3 IAM Policy
+resource "aws_iam_policy" "gh-ec2-ami" {
+  name        = "gh-ec2-ami"
+  path        = "/"
+  description = "gh-ec2-ami policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CopyImage",
+        "ec2:CreateImage",
+        "ec2:CreateKeypair",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteKeyPair",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteVolume",
+        "ec2:DeregisterImage",
+        "ec2:DescribeImageAttribute",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceStatus",
+        "ec2:DescribeRegions",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume",
+        "ec2:GetPasswordData",
+        "ec2:ModifyImageAttribute",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifySnapshotAttribute",
+        "ec2:RegisterImage",
+        "ec2:RunInstances",
+        "ec2:StopInstances",
+        "ec2:TerminateInstances"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
 //CodeDeployServiceRole, will be utilizing the codedeploy service
 resource "aws_iam_role" "CodeDeployServiceRole" {
   name = "CodeDeployServiceRole"
